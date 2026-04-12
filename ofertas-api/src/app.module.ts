7aed0bec7bab2875059b5ff9db1d589de86be85e
juuhadg <juku.jj@gmail.com>
2026-04-12 14:00:49 -0300

    chore(app): configura bootstrap da aplicacao com Mongo, Swagger e modulos

---
+++ ofertas-api/src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ScheduleModule } from '@nestjs/schedule';
+import { AuthModule } from './auth/auth.module';
+import { InterestsModule } from './interests/interests.module';
+import { OffersModule } from './offers/offers.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ScheduleModule.forRoot(),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.getOrThrow<string>('MONGO_URI'),
+      }),
+    }),
+    AuthModule,
+    OffersModule,
+    InterestsModule,
+  ],
+})
+export class AppModule {}
---
+++ ofertas-api/src/main.ts
@@ -0,0 +1,31 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Ofertas API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  SwaggerModule.setup('api', app, SwaggerModule.createDocument(app, swaggerConfig));
+
+  app.enableCors();
+
+  const config = app.get(ConfigService);
+  const port = config.get<number>('PORT') ?? 3000;
+  await app.listen(port);
+}
+bootstrap();
